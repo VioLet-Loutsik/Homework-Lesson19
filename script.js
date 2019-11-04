609da6f83e25fe411f6961dc639be447ef18c581
VioLet-Loutsik <violeta.lutic@gmail.com>
2019-11-05 00:19:36 +0200

    add my work

---
+++ script.js
@@ -0,0 +1,11 @@
+  elem1.onclick = function() {
+    alert('You clicked First button!');
+  };
+
+  elem2.onclick = function() {
+    alert('You clicked Second button!');
+  };
+
+  elem3.onclick = function() {
+    alert('You clicked Third button!');
+  };
